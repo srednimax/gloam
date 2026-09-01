597f79672f849ad24e7db96e83eed5a26211ea2e
Maksymilian Średniawa <72980576+srednimax@users.noreply.github.com>
2026-09-01 20:36:47 +0200

    chore(main): release 0.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,7 +84,7 @@ android {
         minSdk = 33
         targetSdk = 36
         versionCode = gitVersionCode
-        versionName = "0.3.0" // x-release-please-version
+        versionName = "0.4.0" // x-release-please-version
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
